72547e107976ab5ca2cf1364edf470c7a610b85a
Samuel Marvin <97563317+SLMarv@users.noreply.github.com>
2025-02-01 16:31:40 -0500

    Initial Commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+	import { ref } from 'vue'
+
+	defineProps({
+		item: String,
+	})
+	const count = ref(0);
+</script>
+
+<template>
+	<p>
+		{{ item }}: {{ count }}
+		<button @click="count++">+</button> 
+		<button @click="count--">-</button> 
+	</p>
+</template>
